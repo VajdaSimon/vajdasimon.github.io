8c3bb253c8a531e1ee178a9308da116189de5e77
Vajda Simon <75036456+VajdaSimon@users.noreply.github.com>
2021-10-12 21:07:49 +0200

    commit

---
+++ language/eng.js
@@ -0,0 +1,7 @@
+const szoveg = [];
+
+szoveg["cim"] = "Games in HTML5"
+szoveg["leiras"] = "I make these games as an exercise. Main languages: <b>HTML5</b>, <b>CSS3</b> and <b>JavaScript5</b>"
+szoveg["munkaim"] = "My works so far:"
+szoveg["tankos"] = "A project whit a tank"
+szoveg["madaras"] = "A project whit a \"bird\""
---
+++ language/hun.js
@@ -0,0 +1,7 @@
+const szoveg = [];
+
+szoveg["cim"] = "Játékok HTML5-ben"
+szoveg["leiras"] = "Gyakorlásként készítem ezeket a játékokat. Főbb nyelvek: <b>HTML5</b>, <b>CSS3</b> és <b>JavaScript5</b>"
+szoveg["munkaim"] = "Eddigi munkáim:"
+szoveg["tankos"] = "Tankos"
+szoveg["madaras"] = "Madaras"
